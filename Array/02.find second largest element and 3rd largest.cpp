497aca776d4f8266856c9300513c98485a3ad266
mayur2410-tech <mayurwaykar9@gmail.com>
2025-11-14 00:31:09 +0530

    find second and third largest in array

--- Array/02.find second largest element and 3rd largest.cpp
+++ Array/02.find second largest element and 3rd largest.cpp
@@ -7,17 +7,23 @@ int main() {
    vector<int> arr ={5,3,4,6,1};
    int n = arr.size();
    int secondLargest=0;
+   int thirdLargest=0;
    int Largest = arr[0];
    for(int i = 0 ; i <n ; i++){
        if(arr[i]>Largest){
+           thirdLargest=secondLargest;
           secondLargest=Largest;
            Largest=arr[i];
        }else if(arr[i]> secondLargest && arr[i]!=Largest){
+           thirdLargest=secondLargest;
            secondLargest = arr[i];
+       }else if(thirdLargest<arr[i] && arr[i]!=Largest && arr[i]!=secondLargest){
+           thirdLargest=arr[i];
        }
    }
+   cout<<thirdLargest<<endl;
+  
    cout<<secondLargest<<endl;
    cout<<Largest;
 
     return 0;
-}
