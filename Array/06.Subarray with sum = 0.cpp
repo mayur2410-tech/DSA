3843d87d0ae68e6551183ff4e427077da3f4a298
mayur2410-tech <mayurwaykar9@gmail.com>
2025-11-15 00:02:12 +0530

    subarray with sum 0

---
+++ Array/06.Subarray with sum = 0.cpp
@@ -0,0 +1,38 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+#include <climits>
+using namespace std;
+
+int main() {
+   vector<int> arr = {4, -3, 1, 2, -3};
+   int n = arr.size();
+   bool found = false;
+//   int maxSum =INT_MIN ;
+   for(int i=0; i < n ; i++){
+       int sum =0;
+       for(int j=i; j <n ;j++){
+        //   cout<<arr[i]<<arr[j]<< " ";
+        sum = sum +arr[j];
+     
+        if(sum==0){
+            for(int k=i; k<=j ;k++){
+                cout<<arr[k]<<" ";
+            }
+                cout << endl;
+                found = true;
+                break; 
+          
+            
+        }
+       }
+         
+      
+       if (found) break;
+   }
+   if(!found){
+       cout<<"No subArray";
+   }
+//   cout<<maxSum;
+    return 0;
+}
