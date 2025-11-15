1f20d8ebaf0d91c935a8f08aa3db71c185cf976b
mayur2410-tech <mayurwaykar9@gmail.com>
2025-11-15 14:16:54 +0530

    error

--- Array/09. Maximum average of subarray of size k .cpp
+++ Array/09. Maximum average of subarray of size k .cpp
@@ -7,17 +7,16 @@ int main() {
     vector<int> arr ={1,12,-5,-6,50,3};
     int n = arr.size();
     int k =4;
-    float maxSum = 0;
     int sum =0;
     for(int i =0 ; i < k ; i++){
         sum = sum + arr[i];
     }
-    
+    int maxSum = sum;
     for(int i =k ; i < n ; i++){
-        sum = sum + arr[k] - arr[i-k];
+        sum = sum + arr[i] - arr[i-k];
         maxSum = max(sum,maxSum);
     }
   
-cout<<"average is"<<maxSum/k;
+cout<<"average is "<<maxSum/k;
     return 0;
 }
