1a47e870c101920181b6cde1c16b3e63fe67dec8
mayur2410-tech <mayurwaykar9@gmail.com>
2026-06-07 22:55:37 +0530

    diameter of BT

---
+++ Binary_Tree/DiameterOfTree/diameter.cpp
@@ -0,0 +1,10 @@
+// Diameter of tree = number of nodes in the longest part between two leaves nodes
+
+Approach 1 = O(n^2)
+
+two cases 
+i) diameter goes throught the root 
+    - calculate left hieght + right height + 1;
+
+ii) diameter not goes from root either in left subtree or rightsubtree
+   - 
